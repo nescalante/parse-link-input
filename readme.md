229be17962efd63fe28c0d6f46238a37d095d8e2
Nicolas Escalante <nlante@gmail.com>
2017-05-24 17:24:33 -0300

    :rocket:

---
+++ readme.md
@@ -0,0 +1,18 @@
+# parse-link-input
+
+## Install
+
+```shell
+npm install parse-link-input
+```
+
+## Usage
+
+```js
+const parseLinkInput = require('parse-link-input');
+parseLinkInput('foo "bar"'); // { href: 'http://foo', title: 'bar' }
+```
+
+## License
+
+MIT
---
+++ test/parse.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const parseLinkInput = require('../index');
+
+describe('parse-link-input', () => {
+  it('parses without protocol', () => {
+    const result = parseLinkInput('foo');
+    assert.equal(result.href, 'http://foo');
+    assert.equal(result.title, null);
+  });
+
+  it('parses without protocol', () => {
+    const result = parseLinkInput('ftp://foo');
+    assert.equal(result.href, 'ftp://foo');
+    assert.equal(result.title, null);
+  });
+
+  it('parses with title', () => {
+    const result = parseLinkInput('ftp://foo "bar"');
+    assert.equal(result.href, 'ftp://foo');
+    assert.equal(result.title, 'bar');
+  });
+});
